051ad0a905609f12db7a1035dee658e06a5f5fb2
0xseppuk <0xseppuk@gmail.com>
2026-02-08 17:42:36 +0300

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,19 @@ export default function RootLayout({
           }}
           type="application/ld+json"
         />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href="/favicon-180.png" rel="apple-touch-icon" sizes="180x180" />
       </head>
       <body
         className={clsx(
